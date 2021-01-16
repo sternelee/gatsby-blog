7063153ed9ad1d7c97f13fc99c301d3df9d24f47
Sterne Lee <sternelee@gmail.com>
2021-01-16 16:44:04 +0800

    �更新结构

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages/weekly`,
+        path: `${__dirname}/src/pages/_source`,
         name: `markdown-pages`,
       },
     },
---
+++ src/data/weeks.ts
@@ -0,0 +1,98 @@
+// 程序猿周报 目录映射
+
+const WeeksMap = {
+  1: {
+    uri: "https://javascriptweekly.com",
+    title: "JavaScript Weekly",
+  },
+  2: {
+    uri: "https://frontendfoc.us",
+    title: "Frontend Focus",
+  },
+  3: {
+    uri: "https://nodeweekly.com",
+    title: "Node Weekly",
+  },
+  4: {
+    uri: "https://react.statuscode.com",
+    title: "React Status", // 要重新匹配
+  },
+  5: {
+    uri: "https://mobiledevweekly.com",
+    title: "Mobile Developer Weekly",
+  },
+  6: {
+    uri: "https://golangweekly.com",
+    title: "Golang Weekly",
+  },
+  7: {
+    uri: "https://rubyweekly.com",
+    title: "Ruby Weekly",
+  },
+  8: {
+    uri: "https://dbweekly.com",
+    title: "Database Status",
+  },
+  9: {
+    uri: "https://weekly.statuscode.com",
+    title: "StatusCode Weekly", // 要重新匹配
+  },
+  10: {
+    uri: "https://serverless.email",
+    title: "Serverless Status", // 要重新匹配
+  },
+  11: {
+    uri: "https://mongodb.email",
+    title: "MongoDB Memo",
+  },
+  12: {
+    uri: "https://postgresweekly.com",
+    title: "Postgres Weekly",
+  },
+  13: {
+    uri: "https://denoweekly.com",
+    title: "Deno Weekly",
+  },
+  21: {
+    uri: "https://pycoders.com", // TODO: 跳转链接需要特殊处理, 翻译时链接个数不匹配
+    title: "Python Weekly",
+  },
+  22: {
+    uri: "https://news.vuejs.org",
+    title: "Vuejs News",
+  },
+  23: {
+    uri: "https://css-weekly.com",
+    title: "CSS Weekly",
+  },
+  24: {
+    uri: "https://ma.ttias.be/cronweekly",
+    title: "cron.weekly",
+  },
+  25: {
+    uri: "https://webtoolsweekly.com",
+    title: "Web Tools Weekly",
+  },
+  26: {
+    uri: "https://this-week-in-rust.org",
+    title: "Rust Weekly",
+  },
+  27: {
+    uri: "https://rust-gamedev.github.io",
+    title: "Rust Game Dev",
+  },
+  28: {
+    uri: "https://androidweekly.net",
+    title: "Android Weekly",
+  },
+  29: {
+    uri: "https://iosdevweekly.com",
+    title: "IOS Dev Weekly",
+  },
+  30: {
+    uri: "https://daily.dev",
+    title: "Daily开发者周报"
+  }
+};
+
+export default WeeksMap
